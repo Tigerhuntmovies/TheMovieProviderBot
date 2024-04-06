312b0dd39a82b441a6b39128d7c38feb4391567b
Tigerhuntmovies <162421228+Tigerhuntmovies@users.noreply.github.com>
2024-04-06 18:25:44 +0530

    Delete plugins/http: directory

--- plugins/http:/tools.py
+++
@@ -1,6 +0,0 @@
-async def is_user_on_chat(bot: TelegramClient, chat_id: int, user_id: int) -> bool:
-    try:
-        check = await bot.get_permissions(chat_id, user_id)
-        return check
-    except:
-        return False
