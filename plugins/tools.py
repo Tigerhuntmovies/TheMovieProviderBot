1cba83fb06c74bbafb9578b812dae058862e4b40
Tigerhuntmovies <162421228+Tigerhuntmovies@users.noreply.github.com>
2024-04-24 00:24:36 +0530

    Delete plugins/tools.py

--- plugins/tools.py
+++
@@ -1,7 +0,0 @@
-async def is_user_on_chat(client, chat_id: int, user_id: int) -> bool:
- 
-    try:
-        chat_member = await client.get_chat_member(chat_id, user_id)
-        return True if chat_member else False
-    except:
-        return False
